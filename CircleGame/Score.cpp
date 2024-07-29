8dcfac37518ae9204848974ddbe87381f6b1d711
ThatCreeper <creeperhostanimations@gmail.com>
2024-07-29 16:13:33 -0500

    Slightly more elaborate high score ticker

--- CircleGame/Score.cpp
+++ CircleGame/Score.cpp
@@ -23,22 +23,40 @@ void Score::tick(float delta, const Collisions &collisions)
 	}
 }
 
-void Score::tickCounters(float delta, bool moved)
-{
-	highscorecount += delta * (moved ? 90 : 300);
-	streakccount += delta * ((streakc - streakccount > 300) ? 140 : 90);
+static void tickHighScoreCounter(float delta, bool moved, float &highscorecount, float &highscore) {
+	if (highscorecount == highscore)
+		return;
+
+	float speed = 90;
+	
+	float diff = highscore - highscorecount;
+	if (diff > 1000)
+		speed = 600;
+	else if (diff > 300)
+		speed = 140;
+
+	if (!moved)
+		speed *= 2;
+
+	highscorecount += speed * delta;
 	if (highscorecount > highscore)
 		highscorecount = highscore;
+}
+
+void Score::tickCounters(float delta, bool moved)
+{
+	tickHighScoreCounter(delta, moved, highscorecount, highscore);
+	streakccount += delta * ((streakc - streakccount > 300) ? 140 : 90);
 	if (streakccount > streakc)
 		streakccount = streakc;
 	
 	if (score > scorecount) {
-		scorecount += delta * ((score - scorecount > 300) ? 140 : 90);
+		scorecount += delta * ((score - scorecount > 300) ? 140 : 90) * (moved ? 1 : 4);
 		if (scorecount > score)
 			scorecount = score;
 	}
 	else if (score < scorecount) {
-		scorecount -= delta * ((scorecount - score > 30) ? 300 : 20);
+		scorecount -= delta * ((scorecount - score > 30) ? 300 : 20) * (moved ? 1 : 4);
 		if (scorecount < score)
 			scorecount = score;
 	}
